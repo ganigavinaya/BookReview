fb047de82da3efb8b2b77077fa26c22bf833db02
maddy3019 <madhura2205@yahoo.com>
2017-11-21 03:40:51 -0600

    Adding validation for empty fields and error message update.

--- src/client/app/register/register.component.ts
+++ src/client/app/register/register.component.ts
@@ -3,8 +3,6 @@ import {Router} from '@angular/router';
 import {UserService} from '../services/user.service';
 import {User} from '../models/user';
 
-declare var jquery: any;
-declare var $: any;
 
 @Component({
   selector: 'app-register',
@@ -12,21 +10,35 @@ declare var $: any;
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  userdata: any = {};
+  userdata: User = {_id : '', name: '', email: '', username: '', password: ''};
 
   constructor(private router: Router,
               private userService: UserService) { }
 
   register() {
-    this.userService.createUser(this.userdata)
-      .subscribe(
+
+    this.userService.getAllUsers().subscribe(
         data => {
-          this.router.navigate(['/home']);
+          if (!(this.userdata.email == '' || this.userdata.name == '' || this.userdata.username == '' || this.userdata.password == '')) {
+            this.userService.createUser(this.userdata)
+              .subscribe(
+                data => {
+                  this.router.navigate(['/home']);
+                },
+                error => {
+                  var text = document.getElementById('error');
+                  text.innerText = 'Email ID/Username already exists!';
+                  console.log('registration error here');
+                });
+          } else {
+            var text = document.getElementById('error');
+            text.innerText = 'Enter all fields!';
+          }
+
         },
         error => {
-          console.log(error);
+          console.log('registration error ');
         });
   }
-
 
 }
